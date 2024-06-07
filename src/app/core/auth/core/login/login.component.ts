e3115a1267635251c8e909b60c033f9e2a3811f7
Dominik <lolkowskid@gmail.com>
2024-06-07 22:59:37 +0200

    login and register

--- src/app/core/auth/core/login/login.component.ts
+++ src/app/core/auth/core/login/login.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ MatFormFieldModule, MatInputModule, MatButton ],
+  imports: [ MatFormFieldModule, MatInputModule, MatButton, RouterLink ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/core/auth/core/register/register.component.ts
+++ src/app/core/auth/core/register/register.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { MatInput } from '@angular/material/input';
     MatFormField,
     MatInput,
     MatLabel,
+    RouterLink,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
