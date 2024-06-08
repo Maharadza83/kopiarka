281d0caffda482b303d1441067c4aeb349fd4b52
Igor <Igor.patkowski7@gmail.com>
2024-06-08 14:38:31 +0200

    Menu-routing-not-responsive

--- src/app/shell/components/menu/menu.component.ts
+++ src/app/shell/components/menu/menu.component.ts
@@ -1,11 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [
     NgClass,
+    MatButton,
+    MatMenu,
+    MatMenuTrigger,
+    MatMenuItem,
+    RouterLink,
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
