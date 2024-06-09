6aa681b1c7e22537e70b22d1d74d430de6cffd66
blazej.klopotek <bklopotek@9bits.pl>
2024-06-09 22:20:22 +0200

    add auth

--- src/app/guards/is-logged-out.guard.ts
+++ src/app/guards/is-logged-out.guard.ts
@@ -1,11 +1,17 @@
 import { ActivatedRouteSnapshot, CanActivateFn, Router, UrlTree } from '@angular/router';
 import { inject } from '@angular/core';
-import {catchError, map, Observable, of} from 'rxjs';
-import {AuthService} from "../services/auth.service";
+import { catchError, map, Observable, of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { UserStore } from '@copy/store/user.store';
 
-export const isLoggedOutGuard: CanActivateFn = (_route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree   => {
+export const isLoggedOutGuard: CanActivateFn = (_route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  const userStore = inject(UserStore);
+
+  if (userStore.user() === null) {
+    return true;
+  }
 
   return authService.getSelf().pipe(
     map((user) => {
@@ -16,7 +22,7 @@ export const isLoggedOutGuard: CanActivateFn = (_route: ActivatedRouteSnapshot):
       }
     }),
     catchError(() => {
-      return of(true)
-    })
+      return of(true);
+    }),
   );
 };
--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
+global['localStorage'] = localStorage;
+
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
